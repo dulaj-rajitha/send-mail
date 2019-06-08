69eed931a0e251734d4c9ce889df90ba73943935
dulaj-rajitha <dulaj.rajitha.12@cse.mrt.ac.lk>
2019-06-08 21:06:01 +0530

    logger

--- mail-sender-commons/src/main/java/support/CommonLogger.java
+++ mail-sender-commons/src/main/java/support/CommonLogger.java
@@ -1,15 +1,45 @@
 package support;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Dulaj on 2019-06-08.
  */
-public class CommonLogger {
+public final class CommonLogger {
+    private static final Logger LOGGER;
+    
+    static {
+        LOGGER = LoggerFactory.getLogger(CommonLogger.class);
+    }
+    
+    private CommonLogger() {
+    }
+    
+    private static Logger getLogger(Object origin) {
+        if (origin instanceof Class<?>) {
+            return LoggerFactory.getLogger((Class<?>) origin);
+        } else {
+            return LoggerFactory.getLogger(origin.getClass());
+        }
+    }
     
     public static void logInfoMessage(String message) {
-        System.out.println(message);
+        LOGGER.info(message);
     }
     
-    public static void logErrorMessage(Throwable throwable) {
-        System.err.println(throwable.getMessage());
+    public static void logInfoMessage(Object origin, String message, Object... args) {
+        getLogger(origin).info(message, args);
     }
+    
+    
+    public static void logErrorMessage(Throwable throwable) {
+        LOGGER.error(throwable.getMessage(), throwable);
+    }
+    
+    public static void logErrorMessage(Object origin, String message, Throwable args) {
+        getLogger(origin).error(message, args);
+    }
+    
 }
