ef59a227cab01263809e14670e87b903a6b95e4f
dulaj-rajitha <dulaj.rajitha.12@cse.mrt.ac.lk>
2019-06-08 19:00:59 +0530

    jackson data serialization

--- mail-sender-commons/src/main/java/util/DataConversionUtil.java
+++ mail-sender-commons/src/main/java/util/DataConversionUtil.java
@@ -1,21 +1,63 @@
 package util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import model.SendEmail;
 import model.SendEmailAck;
+import support.CommonLogger;
+
+import java.io.IOException;
 
 /**
  * Created by Dulaj on 2019-06-08.
  */
 public final class DataConversionUtil {
+    
+    private static final ObjectMapper objectMapper;
+    
+    static {
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+    
     private DataConversionUtil() {
         //        util class with that cannot be instantiated
     }
     
-    public static SendEmail convertToSendEmail(Object input) {
-        return null;
+    public static SendEmail convertToSendEmail(String input) {
+        try {
+            return objectMapper.readValue(input, SendEmail.class);
+        } catch (IOException e) {
+            CommonLogger.logErrorMessage(e);
+            return null;
+        }
     }
     
-    public static SendEmailAck convertToSendEmailAck(Object input) {
-        return null;
+    public static String getJsonString(SendEmail sendEmail) {
+        try {
+            return objectMapper.writeValueAsString(sendEmail);
+        } catch (JsonProcessingException e) {
+            CommonLogger.logErrorMessage(e);
+            return null;
+        }
+    }
+    
+    public static SendEmailAck convertToSendEmailAck(String input) {
+        try {
+            return objectMapper.readValue(input, SendEmailAck.class);
+        } catch (IOException e) {
+            CommonLogger.logErrorMessage(e);
+            return null;
+        }
+    }
+    
+    public static String getJsonString(SendEmailAck sendEmailAck) {
+        try {
+            return objectMapper.writeValueAsString(sendEmailAck);
+        } catch (JsonProcessingException e) {
+            CommonLogger.logErrorMessage(e);
+            return null;
+        }
     }
 }
