69bd19af85137af83f7feac15fdb938444f05779
dulaj-rajitha <dulaj.rajitha.12@cse.mrt.ac.lk>
2019-06-08 14:50:46 +0530

    data conversions in common project: to be implemented

---
+++ mail-sender-commons/src/main/java/util/DataConversionUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import model.SendEmail;
+import model.SendEmailAck;
+
+/**
+ * Created by Dulaj on 2019-06-08.
+ */
+public final class DataConversionUtil {
+    private DataConversionUtil() {
+        //        util class with that cannot be instantiated
+    }
+    
+    public static SendEmail convertToSendEmail(Object input) {
+        return null;
+    }
+    
+    public static SendEmailAck convertToSendEmailAck(Object input) {
+        return null;
+    }
+}
