a9ed6507ae568d85f6821b9557e669f43c19b183
dulaj-rajitha <dulaj.rajitha.12@cse.mrt.ac.lk>
2019-06-09 14:27:26 +0530

    crate new threads for handle multiple socket connections to send mail service

--- email-sender/src/main/java/app/EmailSender.java
+++ email-sender/src/main/java/app/EmailSender.java
@@ -1,21 +1,15 @@
 package app;
 
-import model.SendEmail;
-import model.SendEmailAck;
 import support.CommonLogger;
-import util.DataConversionUtil;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.Properties;
 
 /**
  * Created by Dulaj on 2019-06-08.
  */
-public class EmailSender implements Runnable {
+public class EmailSender {
     
     private static final String SERVICE_PORT_KEY = "SERVICE_PORT";
     private static final String DEFAULT_PORT = "4444";
@@ -25,7 +19,7 @@ public class EmailSender implements Runnable {
     private static final String DEFAULT_MAIL_SERVER_PORT = "25";
     
     private int servicePort;
-    private MailServerConnector mailServerConnector;
+    private Properties mailConfigurations;
     private boolean running;
     
     public EmailSender() {
@@ -34,58 +28,39 @@ public class EmailSender implements Runnable {
         String mailServerHost = System.getProperty(MAIL_SERVER_HOST_KEY, DEFAULT_MAIL_SERVER_HOST);
         String mailServerPort = System.getProperty(MAIL_SERVER_POST_KEY, DEFAULT_MAIL_SERVER_PORT);
         
-        Properties mailConfigurations = new Properties();
+        mailConfigurations = new Properties();
         mailConfigurations.put("mail.smtp.auth", "false");
         mailConfigurations.put("mail.smtp.starttls.enable", "true");
         mailConfigurations.put("mail.smtp.host", mailServerHost);
         mailConfigurations.put("mail.smtp.port", mailServerPort);
         
-        mailServerConnector = new MailServerConnector(mailConfigurations);
-        
         running = true;
+    }
+    
+    public static void main(String[] args) {
+        EmailSender emailSender = new EmailSender();
+        emailSender.startServer();
     }
     
     private void startServer() {
         
-        CommonLogger.logInfoMessage("starting mail sender service");
-        CommonLogger.logInfoMessage("starting listening on port : " + servicePort);
+        CommonLogger.logInfoMessage(this, "starting mail sender service");
+        CommonLogger.logInfoMessage(this, "starting listening on port : " + servicePort);
         
-        
-        while (running) {
-            try (ServerSocket serverSocket = new ServerSocket(servicePort);
-                 Socket socket = serverSocket.accept();
-                 ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-                 ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())
-            ) {
-                
-                CommonLogger.logInfoMessage("receiving a client mail");
-                
-                //  read the mail
-                Object clientData = inputStream.readObject();
-                SendEmail inputMail = DataConversionUtil.convertToSendEmail(clientData);
-                
-                // send the mail through fake SMTP server
-                SendEmailAck mailServerAck = mailServerConnector.sendMailToServer(inputMail);
-                
-                //  write ack to client
-                
-                outputStream.writeObject(mailServerAck);
-                
-            } catch (IOException e) {
-                CommonLogger.logErrorMessage(e);
-            } catch (ClassNotFoundException e) {
-                CommonLogger.logErrorMessage(e);
+        try (ServerSocket serverSocket = new ServerSocket(servicePort)) {
+            while (running) {
+                CommonLogger.logInfoMessage(this, "creating new thread for send the mail with the socket lister");
+                new EmailSenderThread(serverSocket.accept(), mailConfigurations).start();
             }
+        } catch (IOException e) {
+            CommonLogger.logErrorMessage(this, "Could not listen on port " + servicePort, e);
+            System.exit(-1);
         }
     }
     
     public void shutDown() {
-        CommonLogger.logInfoMessage("shutting down mail sender service");
+        CommonLogger.logInfoMessage(this, "shutting down mail sender service");
         running = false;
     }
     
-    @Override
-    public void run() {
-        startServer();
-    }
 }
